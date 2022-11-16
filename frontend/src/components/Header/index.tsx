38a46f5f5fa4fae1f61b7bb9468c7d4dc4113443
Willian Marcal <willianpetermarcal@gmail.com>
2022-11-16 13:07:46 +0000

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="Sales" />
+                <h1>Sales</h1>
+                <p>
+                    Development by Willian Peter Marcal
+                    <a href="https://github.com/WillianMarcal/Sales"></a>
+                </p>
+            </div>
+        </header>
+    )
+}
+export default Header
